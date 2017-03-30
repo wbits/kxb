6401860addf7d9bffe601f7fd84078959141184b
Dick <dick@insided.nl>
2017-03-30 23:02:40 +0200

    changed setup for dockerize

---
+++ containers/phpfpm/entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+shopt -s nocasematch
+
+if [ -z "$REPO" ]; then
+    REPO="https://github.com/wbits/kxb.git"
+fi
+
+WWW="/var/www"
+APP_DIR="app"
+APP_DIRPATH="$WWW/$APP_DIR"
+
+alias
+if [ "$1" == "setup" ] && [ "$2" == "silex" ]; then
+    cd $WWW
+    if  [ -f "$APP_DIR/composer.json" ]; then
+        printf "\x1b[93;41m Silex project already exists \x1b[0m \n";
+        exit 0
+    fi
+    git clone $REPO $APP_DIR
+    cd $APP_DIRPATH
+    composer install -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+elif [ "$1" == "update" ] && [ "$2" == "silex" ]; then
+    cd $APP_DIRPATH
+    git pull origin master
+    composer install -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+elif [ "$1" == "update" ] && [ "$2" == "code" ]; then
+    cd $APP_DIRPATH
+    git pull origin master
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+elif [ "$1" == "composer" ]; then
+    cd $APP_DIRPATH
+    shift
+    composer $@ -vvvvv
+    chown -R www-data:www-data $APP_DIRPATH
+    exit 0
+fi
+
+
+key=$1
+shift
+echo "Running command: $key $@"
+exec $key $@
